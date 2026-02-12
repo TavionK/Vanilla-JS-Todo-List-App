715743e5845a9cfe8eceef2ecf4b38ebf61aa3ae
TavionK <tavion.k.britt99@gmail.com>
2026-02-12 00:07:11 -0500

    "Add delete functionality for todo items and improve input validation"

--- index.js
+++ index.js
@@ -8,6 +8,19 @@ let todoList = [{ id: crypto.randomUUID(), text: "Buy milk" }];
 // Event listener for the addBtn button
 addBtn.addEventListener("click", addTodoItem);
 
+// Event listener to get the id of the clicked item
+document.addEventListener("click", function (event) {
+  if (event.target.closest("button") === null) return;
+  deleteTodoItem(event.target.closest("button").getAttribute("data-id"));
+});
+
+function deleteTodoItem(id) {
+  todoList = todoList.filter(function (item) {
+    return item.id !== id;
+  });
+  render();
+}
+
 // If the user presses enter, add the item to the list
 todoInput.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
@@ -17,6 +30,11 @@ todoInput.addEventListener("keypress", function (event) {
 
 // Function to add item to the list and then call the render function
 function addTodoItem() {
+  if (todoInput.value.trim() === "") {
+    todoInput.focus();
+    todoInput.value = "";
+    return;
+  }
   // creates random id and adds the object to the todoList array
   todoList.push({ id: crypto.randomUUID(), text: todoInput.value });
   // Clears the input field
@@ -29,7 +47,11 @@ function addTodoItem() {
 // This array is supplied to the render method to render the list
 function getTodoItems() {
   return todoList.map(function (item) {
-    return `<li>${item.text}</li>`;
+    return `
+    <li>
+        ${item.text}
+        <button data-id="${item.id}" id="btn" aria-label="delete todo item"><span aria-hidden="true">x</span></button>
+    </li>`;
   });
 }
 
