b940d0ed551e86c7894154c57f2a8d7e0ffe9e21
TavionK <tavion.k.britt99@gmail.com>
2026-02-23 12:09:48 -0500

    "Redesign todo list item layout: apply Tailwind classes for cleaner styling and improved button alignment"

--- src/todos.ts
+++ src/todos.ts
@@ -99,16 +99,16 @@ export function getTodoItems(): string[] {
   if (todoList.length === 0) return [];
   return todoList.map(function (item: Todo): string {
     return `
-    <li>
-        ${item.text}
-        <div class="btn-container">
-            <button data-id="${item.id}" class="btn btn-complete" id="btn" aria-label="mark todo item as complete">
-                <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path d="M434.8 70.1c14.3 10.4 17.5 30.4 7.1 44.7l-256 352c-5.5 7.6-14 12.3-23.4 13.1s-18.5-2.7-25.1-9.3l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l101.5 101.5 234-321.7c10.4-14.3 30.4-17.5 44.7-7.1z"/></svg>
+    <li class="flex place-content-between items-start bg-gray-700 rounded-md mb-4 p-2">  
+        <div class="flex gap-2">
+            <button data-id="${item.id}" class="flex justify-center items-center cursor-pointer rounded-md size-8 p-2 bg-green-800 btn-complete" id="btn" aria-label="mark todo item as complete">
+                <svg class="size-4 fill-white icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path d="M434.8 70.1c14.3 10.4 17.5 30.4 7.1 44.7l-256 352c-5.5 7.6-14 12.3-23.4 13.1s-18.5-2.7-25.1-9.3l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l101.5 101.5 234-321.7c10.4-14.3 30.4-17.5 44.7-7.1z"/></svg>
             </button>
-            <button data-id="${item.id}" class="btn btn-delete" id="btn" aria-label="delete todo item">
-                <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free 7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path d="M232.7 69.9L224 96L128 96C110.3 96 96 110.3 96 128C96 145.7 110.3 160 128 160L512 160C529.7 160 544 145.7 544 128C544 110.3 529.7 96 512 96L416 96L407.3 69.9C402.9 56.8 390.7 48 376.9 48L263.1 48C249.3 48 237.1 56.8 232.7 69.9zM512 208L128 208L149.1 531.1C150.7 556.4 171.7 576 197 576L443 576C468.3 576 489.3 556.4 490.9 531.1L512 208z"/></svg>
-            </button>
+            ${item.text}
         </div>
+        <button data-id="${item.id}" class="flex justify-center items-center cursor-pointer rounded-md size-8 p-2 bg-red-800 btn-delete" id="btn" aria-label="delete todo item">
+            <svg class="size-4 fill-white icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free 7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path d="M232.7 69.9L224 96L128 96C110.3 96 96 110.3 96 128C96 145.7 110.3 160 128 160L512 160C529.7 160 544 145.7 544 128C544 110.3 529.7 96 512 96L416 96L407.3 69.9C402.9 56.8 390.7 48 376.9 48L263.1 48C249.3 48 237.1 56.8 232.7 69.9zM512 208L128 208L149.1 531.1C150.7 556.4 171.7 576 197 576L443 576C468.3 576 489.3 556.4 490.9 531.1L512 208z"/></svg>
+        </button>
     </li>`;
   });
 }
